a4f7f7ec09168ecb30ede4865ca5da30614545fc
Arunav Bhattacharya <itsarunav4u@gmail.com>
2020-09-10 09:58:50 -0700

    Longest Substring Of K Distinct Chars

--- 1-sliding-window/src/main/java/me/arunav/dsalgo/problems/slidingwindow/_4LongestSubstringOfKDistinctChars.java
+++ 1-sliding-window/src/main/java/me/arunav/dsalgo/problems/slidingwindow/_4LongestSubstringOfKDistinctChars.java
@@ -16,11 +16,18 @@ public class _4LongestSubstringOfKDistinctChars {
         int maxLength = 0, startIdx = 0, endIdx = 0;
         char[] charArray = str.toCharArray();
 
+        // Sliding Window Pattern
         for (char c : charArray) {
+            // Load the char in a map and increment the no. of times present by 1
             map.put(c, (map.getOrDefault(c, 0) + 1));
+
+            // If the number of distinct characters (size of map) <= K, then calculate maxLength
             if (map.size() <= K) {
                 maxLength = Math.max(maxLength, endIdx - startIdx + 1);
             }
+
+            // While the  number of distinct characters (size of the map) >= K, then increment the sliding window
+            // starting index and also reduce the count of the chars in that index from the map
             while (map.size() > K) {
                 char startingCharInWindow = charArray[startIdx];
                 int count = map.get(startingCharInWindow);
